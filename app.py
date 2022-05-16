8e98738aba5639b6a6bc0902ef760078d1ef8c64
NEEtojin <105396245+NEEtojin@users.noreply.github.com>
2022-05-17 08:23:33 +0900

    220517

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import os
-from flask          import Flask, flash, render_template, request, redirect, g, Response, jsonify, current_app, session, url_for
+from flask          import (
+        Flask, flash, render_template, request, redirect, session, url_for
+        )
 from werkzeug.utils import secure_filename
+import bcrypt, jwt
 
 import numpy as np
 import json
-from functools import wraps
-import bcrypt, jwt
-from datetime import datetime, timedelta
 from PIL import Image
-#import matplotlib.pyplot as plt
-#%matplotlib inline
 
 import torch
-import torchvision
 from torchvision import models, transforms
 
 HOME_FOLDER = os.getcwd()
@@ -21,8 +18,11 @@ ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__ )
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = 'this is not a secret key'#just for test
-app.users = {'testid':bcrypt.hashpw(b"testpassword",bcrypt.gensalt())}
+app.secret_key = 'this is not a secret key'
+app.users = {
+        'testid':bcrypt.hashpw(b"testpassword",bcrypt.gensalt()),
+        'testid2':bcrypt.hashpw(b"testpassword2",bcrypt.gensalt())        
+        }
 
 net = models.vgg16()
 print('loading model...')
@@ -31,7 +31,6 @@ print('loading model...')
 print('load complete')
 net.eval()
 
-savepath = os.path.join(HOME_FOLDER+'/imagenet_class_index.json')
 
 class BaseTransform():
 
@@ -46,12 +45,13 @@ class BaseTransform():
     def __call__(self,img):
         return self.base_transform(img)
 
-
 resize = 224
 mean = (0.485, 0.456, 0.406)
 std = (0.229, 0.224, 0.225)
 transform = BaseTransform(resize, mean, std)
 
+
+indexsavepath = os.path.join(HOME_FOLDER+'/imagenet_class_index.json')
 
 class ILSVRCPredictor():
     
@@ -64,7 +64,7 @@ class ILSVRCPredictor():
 
         return predicted_label_name
 
-ILSVRC_class_index = json.load(open(savepath,'r'))
+ILSVRC_class_index = json.load(open(indexsavepath,'r'))
 predictor = ILSVRCPredictor(ILSVRC_class_index)
 
 
@@ -74,6 +74,8 @@ def allowed_file(filename):
 
 @app.route('/', methods = ['GET'])
 def index():
+    #print(session)
+    #print(request.cookies)
     if 'username' in session:
         return f'hello, {session["username"]}!'
     return 'hello!', 200
@@ -82,14 +84,15 @@ def index():
 def login():
     if request.method == 'POST':
         
-        
         username = request.form['username']
         password = request.form['password']
 
-        if bcrypt.checkpw(password.encode('UTF-8'), app.users['testid']):
+        if bcrypt.checkpw(password.encode('UTF-8'), app.users[username]):
             session['username'] = request.form['username']
             session.permanent = True
-            return redirect(url_for('upload_file'))
+            response = redirect(url_for('upload_file'))
+            response.set_cookie('username',username)
+            return response
 
         else:
             return "", 401
@@ -103,13 +106,10 @@ def login():
                 <p><input type=submit value=Login>
             </form>
         '''
-    #return render_template('login.html')
 
 @app.route('/upload', methods = ['GET','POST'])
 def upload_file():
-    #test_session = session(permanent = True)
     if request.method == 'POST':
-        #test_session = session(permanent = True)
         result = ''
         if 'file' not in request.files:
             flash('No file part')
@@ -121,7 +121,6 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            #return filename
             filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
             file.save(filepath)
             img = Image.open(filepath)
@@ -136,12 +135,13 @@ def upload_file():
         else:
             flash('Allowed file type: png, jpg, jpeg, gif')
         return f'This is an image of {result}', 200
-    if 'username' in session:
+    if 'username' in session and session['username']=='testid':
         return render_template('upload.html')
     else:
-        return 'You are not logged in', 401
+        return 'You are not testid', 401
 
 @app.route('/logout')
 def logout():
     session.pop('username',None)
+    print('logout',session)
     return redirect(url_for('index'))
